74c1cc7091b6a998eee404ba275a2e0352815c87
Clara COTTIN <ccottin@e1r5p4.clusters.42paris.fr>
2022-10-01 05:13:36 +0200

    ex00 seems good

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -24,7 +24,7 @@ int	main(void)
 	one.attack("Georges");
 	one.attack("Georges");
 	one.attack("Georges");
-	three = two;
+	three = one;
 	three.attack("Nothing");
 	two.takeDamage(11);
 	two.beRepaired(10);
